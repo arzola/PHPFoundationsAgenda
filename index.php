164d4cb7d24274d1f4e98d01ea4266638ec70152
Oscar Arzola <oscararzola@gmail.com>
2014-11-15 00:09:04 -0600

    Create index.php
    Init file

---
+++ index.php
@@ -0,0 +1,23 @@
+<html> 
+    <head>
+        <title>Pagina principal</title> 
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    </head>
+    <body> 
+        <div class="container">
+            <p class="well">
+                Pagina principal <?php echo 'Cool'; ?>
+            </p> 
+            <table class="table">
+                <tr>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Telefono</th>
+                    <th>Twitter</th>
+                </tr>
+            </table>
+        </div>
+    </body>
+</html>
